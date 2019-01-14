f67765d17becd7923eca20cafa06602001018117
Hamza El Yousfi <hamza.elyousfi@nlight.io>
2019-01-14 10:55:52 +0100

    Initial Commit

---
+++ WebRTC-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebRTC-iOS
+//
+//  Created by Hamza El Yousfi on 1/14/19.
+//  Copyright © 2019 Hamza El Yousfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
